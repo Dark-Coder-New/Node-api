672a7e485710e1c0d438f42eb0f65beb1e86a957
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-06-16 13:02:28 +0530

    auth

--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,16 @@
 // import mongoose from "mongoose";
 // const user =  mongoose.model("User");
 // import bcrypt from "bcrypt";
+// import {v4 as uuid4} from 'uuid';
+// import isLoggedIn from "../middlewares/isLoggedIn.js";
 
 const express = require('express');
 const authRouter =  express.Router()
 const mongoose = require("mongoose");
 const user =  mongoose.model("User");
 const bcrypt = require("bcrypt");
+const { v4:uuid4 } = require('uuid'); 
+const isLoggedIn = require("../middlewares/isLoggedIn.js");
 
 
 
@@ -97,7 +101,17 @@ authRouter.post("/login", (req, res) => {
                      if(result == false){
                         return res.send({error: "Invalid password"})
                      }
-                    return res.send({success: true, message: "User logged in successfully", data: foundUser})
+                      // generate token:  
+                      let token = uuid4();
+                      foundUser.token = token;
+                      foundUser.save()
+                      .then((savedUser)=>{
+                        return res.send({success: true, message: "User logged in successfully", data: savedUser})
+                      })
+
+                      .catch(err=>console.log("issue while saving token in database",err))
+
+                    
                 })
 
         })
@@ -108,6 +122,28 @@ authRouter.post("/login", (req, res) => {
 
 
 })
+
+
+authRouter.get("/secret1",isLoggedIn, (req, res)=>{
+    //  check if user is logged in or not
+     return res.send({success: true, message: "You are authorized to be a Raw Agent", loggedInAgentDetails: req.user})
+})
+
+
+
+authRouter.delete("/logout", isLoggedIn, async(req, res)=>{
+     req.user.token = null;
+     try{
+        let savedUser = await req.user.save()
+        return res.send({success: true, message: "User logged out successfully", data: savedUser})
+     }
+      catch(err){
+        console.log("Logout Failed",err)
+      }
+
+})
+
+
 
 
 // export default authRouter;
